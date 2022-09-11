21bff026509a0aa82dcb207eaac39008127382a0
SoaresPT <soares_123@msn.com>
2022-09-11 21:25:35 +0300

    Update exercises_6_7_8.py

--- exercises_6_7_8.py
+++ exercises_6_7_8.py
@@ -1,6 +1,7 @@
-# 6.1
 import random
+import math
 
+# 6.1
 def dice_roll():
     return random.randint(1, 6)
 
@@ -55,3 +56,25 @@ lista = [0,1,2,3,4,5,6,7,8,9,10,11,13,15,16]
 print(f"Original List: {lista}")
 print(f"Cut-down List: {trim_odd_numbers(lista)}")
 
+# 6.6
+def pizza_price(diameter: float, price: float):
+    # A = Pi(r²)
+    cm2 = math.pi*(diameter/2)**2
+    return price/cm2
+
+p1_size = float(input("Input the size of the pizza #1 in cm: "))
+p1_cost = float(input("Input the price of the pizza #1 in €: "))
+p1_cost_in_m2 = pizza_price(p1_size,p1_cost*10000)
+
+p2_size = float(input("Input the size of the pizza #2 in cm: "))
+p2_cost = float(input("Input the price of the pizza #2 in €: "))
+p2_cost_in_m2 = pizza_price(p2_size,p2_cost*10000)
+
+print(f"{p1_size/100}m² of pizza costs : {p1_cost_in_m2:.2f}€")
+print(f"{p2_size/100}m² of pizza costs : {p2_cost_in_m2:.2f}€")
+if pizza_price(p1_size,p1_cost) > pizza_price(p2_size, p2_cost):
+    print("Pizza 2 provides better value for the money")
+elif pizza_price(p1_size,p1_cost) < pizza_price(p2_size, p2_cost):
+    print("Pizza 1 provides better value for the money")
+else:
+    print("Both pizzas provide the same value for the money")
