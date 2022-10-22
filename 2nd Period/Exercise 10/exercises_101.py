0a6918c0cb6f576f9c4810b5020e6b7623725c95
Soares <soares_123@msn.com>
2022-10-22 19:24:46 +0300

    101

--- 2nd Period/Exercise 10/exercises_101.py
+++ 2nd Period/Exercise 10/exercises_101.py
@@ -1,6 +1,35 @@
 # 10.1
 class Elevator:
-    pass
+    
+    def __init__(self, bottom_floor: int, top_floor: int):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.current_floor = bottom_floor
+    
+    def go_to_floor(self, floor: int):
+        # Check if the arg passed to this method is a valid floor [Not asked by the exercise but figured it would be a nice addiction]
+        if floor > self.top_floor or floor < self.bottom_floor:
+            print("Invalid Floor")
+            return
+
+        if floor < self.current_floor:
+            while floor < self.current_floor:
+                self.floor_down()
+                print(f"The elevator is on the floor : {self.current_floor}")
+            return
+        else:
+            while floor > self.current_floor:
+                self.floor_up()
+                print(f"The elevator is on the floor : {self.current_floor}")
+            return
+
+    def floor_up(self):
+        self.current_floor += 1
+
+    def floor_down(self):
+        self.current_floor -= 1
 
 if __name__ == "__main__":
-    pass
+    h = Elevator(1, 5)
+    h.go_to_floor(5)
+    h.go_to_floor(h.bottom_floor)
