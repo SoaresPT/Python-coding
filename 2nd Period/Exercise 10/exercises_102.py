505062ede77a538ca38991a205f3cb4aa689866f
Soares <soares_123@msn.com>
2022-10-24 18:29:54 +0300

    10.2

--- 2nd Period/Exercise 10/exercises_102.py
+++ 2nd Period/Exercise 10/exercises_102.py
@@ -28,7 +28,29 @@ class Elevator:
         if self.current_floor > self.bottom_floor:
             self.current_floor -= 1
 
+class Building:
+    def __init__(self, bottom_floor: int, top_floor: int, no_elevators: int):
+        self.bottom_floor = bottom_floor
+        self.top_floor = top_floor
+        self.no_elevators = no_elevators
+        self.list_of_elevators = []
+    
+    def create_elevators(self):
+        for i in range(1, self.no_elevators+1):
+            elevator = "elevator" + str(i)
+            elevator = Elevator(self.bottom_floor, self.top_floor)
+            self.list_of_elevators.append(elevator)
+    
+    def run_elevator(self, no_elevator: int, floor: int):
+        self.list_of_elevators[no_elevator-1].go_to_floor(floor)
+
 if __name__ == "__main__":
-    h = Elevator(1, 5)
-    h.go_to_floor(5)
-    h.go_to_floor(h.bottom_floor)
+    #building with 1-10 floors, 4 elevators
+    building = Building(1, 10, 4)
+
+    building.create_elevators()
+    print(building.list_of_elevators)
+    print()
+    print(building.list_of_elevators[0].current_floor)
+    building.run_elevator(1,5)
+    print(building.list_of_elevators[0].current_floor)
