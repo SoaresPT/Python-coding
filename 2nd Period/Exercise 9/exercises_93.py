61407a9e2efcd99430531eee4e83d2025540fd22
SoaresPT <soares_123@msn.com>
2022-10-28 22:34:42 +0300

    Update exercises_93.py

--- 2nd Period/Exercise 9/exercises_93.py
+++ 2nd Period/Exercise 9/exercises_93.py
@@ -7,16 +7,11 @@ class Car:
         self.travelled_distance = travelled_distance
     
     def accelerate(self, km_h):
-        if km_h < 0:
-            if self.current_speed - km_h <= 0:
-                self.current_speed += km_h
-            else:
-                self.current_speed = 0
-        if km_h > 0:
-            if self.current_speed + km_h <= self.maximum_speed:
-                self.current_speed += km_h
-            else:
-                self.current_speed = self.maximum_speed
+        self.current_speed += km_h
+        if self.current_speed >= self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        elif self.current_speed <= 0:
+            self.current_speed = 0
     
     def drive(self, hours_driven):
         self.travelled_distance += hours_driven * self.current_speed
